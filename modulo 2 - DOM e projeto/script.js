78bd2137c67323b8d37579db5b9e0bab99aebee7
Marcelo <marcelo.murakami100@gmail.com>
2024-12-03 17:37:50 -0300

    atualizado

--- modulo 2 - DOM e projeto/script.js
+++ modulo 2 - DOM e projeto/script.js
@@ -10,14 +10,27 @@ document.querySelector('.guess').value;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
+const number = Math.floor(Math.random() * 20 + 1);
+let tentativas = 20;
 document.querySelector('.number').textContent = number;
 
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess'));
+document.querySelector('.btn.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
-    if (!guess) {
-        document.querySelector('.message').textContent = '🛑 No number'
+    if (!guess){
+        document.querySelector('.message').textContent = '😥 You have to input a number!';
+    } else if (guess === number){
+        document.querySelector('.message').textContent = '😍 You WON! Congratilations';
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.width = '25rem';
+    } else if (guess > number) {
+        document.querySelector('.message').textContent = '⬆ Too high!';
+        tentativas--
+        document.querySelector('.score').value = tentativas;
+    } else {
+        document.querySelector('.message').textContent = '⬇ Too low!;'
+        tentativas--
+        document.querySelector('.score').value = tentativas;
     }
 })
