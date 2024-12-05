013a00f6a2f18e7cd5907e40c9b17fa024bd7f55
Your Name <you@example.com>
2024-12-04 22:51:09 -0300

    atualizado

--- modulo 2 - DOM e projeto/Demo MODAL WINDOW/script.js
+++ modulo 2 - DOM e projeto/Demo MODAL WINDOW/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let a = 0; a < btnsShowModal.length; a++) {
+    btnsShowModal[a].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+const close = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+overlay.addEventListener('click', close);
+btnCloseModal.addEventListener('click', close);
